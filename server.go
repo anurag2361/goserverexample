ba6a7d0d77f508609c56213889779112c4ed2af3
anurag <sharmaanurag.236@gmail.com>
2017-10-28 18:05:45 +0530

    added server message

--- server.go
+++ server.go
@@ -2,9 +2,18 @@ package main
 
 import (
         "net/http"
+        "fmt"
+        "log"
+        "bytes"
 )
 
 func main() {
+        var(
+                buf bytes.Buffer
+                logger=log.New(&buf,"Hello: ",log.Lshortfile)
+        )
+        logger.Print("The server runs on localhost:3000")
+        fmt.Print(&buf)
         http.Handle("/", http.FileServer(http.Dir("./static")))
         http.ListenAndServe(":3000", nil)
 }
